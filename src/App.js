642f1893957baa568a3993e51ba44cee72889f73
Philipp Aubert <p.b.m.aubert@gmail.com>
2021-04-10 00:34:42 +0200

    Load Api and setup CSS

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         </header>
         <main className="Main-Grid">
           <Switch>
-            <Route path="/characters/id">
+            <Route path="/characters/:id">
               <CharactersId />
             </Route>
 
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,15 +1,57 @@
 import "./Characters.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-function Characters() {
+export default function Characters() {
+  const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+  const [allPages, setAllPages] = useState();
+
+  useEffect(() => {
+    const url = `https://rickandmortyapi.com/api/character/?page=${page}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setCharacters((initialCharacter) => {
+          return [...initialCharacter, ...data.results];
+        });
+        setAllPages(data.info.pages);
+      });
+  }, [page]);
+
+  function handleLoadMore() {
+    if (page < allPages) {
+      setPage(page + 1);
+    }
+  }
+
+  function createCharacters() {
+    return characters.map((character) => {
+      const { id, name, image } = character;
+      return (
+        <div key={id}>
+          <Link to={`/characters/${id}`}>
+            <div class="Character-Grid">
+              <figure>
+                <img className="Character-Image" src={image} alt={name} />
+                <figcaption className="Character-Text">{name}</figcaption>
+              </figure>
+            </div>
+          </Link>
+        </div>
+      );
+    });
+  }
+
   return (
     <div>
-      <h3 className="Character-Text">All "Rick and Morty" Characters</h3>
-
-      <div className="API-Content"></div>
-
-      <button className="MoreButton">MORE</button>
+      <h3>All Rick and Morty Characters</h3>
+      <section>{createCharacters()}</section>
+      <button className="MoreButton" onClick={handleLoadMore}>
+        {" "}
+        Load More{" "}
+      </button>
     </div>
   );
 }
-
-export default Characters;
